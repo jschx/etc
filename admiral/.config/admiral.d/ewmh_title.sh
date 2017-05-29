e7b19b478ef23474fe094cc831fd6bc5b5b91262
jschx <me@schil.li>
2017-05-28 22:43:27 -0500

    Update window title on a timer until I find a better solution

--- admiral/.config/admiral.d/ewmh_title.sh
+++ admiral/.config/admiral.d/ewmh_title.sh
@@ -1,17 +1,15 @@
-#!/usr/bin/env bash
+#!/usr/bin/env dash
 # ewmh_title.sh - output current window title
 
 . ~/.config/lime/colors.conf
 
-while read -r line; do
-	winid="$(xprop -root _NET_ACTIVE_WINDOW | grep -o "0x.*")"
-	if [ "${winid}" = '0x0' ]; then
-		title=''
-	else
-		title="$(xprop -id "${winid}" WM_NAME | grep -o "\".*\"" | tr -d '"')"
-	fi
-	if [ "${#title}" -ge 61 ]; then
-		title="$(echo ${title} | cut -c-61)..."
-	fi
-	echo "%{B-}%{F$c7}${title}"
-done < <(xprop -root -spy)
+winid="$(xprop -root _NET_ACTIVE_WINDOW | grep -o "0x.*")"
+if [ "${winid}" = '0x0' ]; then
+	title=''
+else
+	title="$(xprop -id "${winid}" WM_NAME | grep -o "\".*\"" | tr -d '"')"
+fi
+if [ "${#title}" -ge 61 ]; then
+	title="$(echo ${title} | cut -c-61)..."
+fi
+echo "%{B-}%{F$c7}${title}"
