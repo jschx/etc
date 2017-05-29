0c73786b5c83b3f77be947b3aea6321639bc367e
jschx <me@schil.li>
2017-05-28 22:45:32 -0500

    Use dash instead of bash

--- admiral/.config/admiral.d/ewmh_workspaces.sh
+++ admiral/.config/admiral.d/ewmh_workspaces.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/env dash
 # ewmh_workspaces.sh - output workspaces
 
 . ~/.config/lime/colors.conf
 
-while read -r line; do
+xprop -root -spy | while read -r line; do
 #	for workspace in $(jot $(xprop -root _NET_NUMBER_OF_DESKTOPS | awk '{print $3}')); do
 	for workspace in 1 2 3 4; do
 		if [ "$(xprop -root _NET_CURRENT_DESKTOP | awk '{print $3}')" = "$(( workspace - 1 ))" ]; then
@@ -13,4 +13,4 @@ while read -r line; do
 		fi
 	done
 	echo
-done < <(xprop -root -spy)
+done
