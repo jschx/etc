4d6b2c2db7e8f9431fed42dd1491a49e9ad3cb6f
jschx <me@schil.li>
2017-05-28 22:36:29 -0500

    Add charging indicator

--- admiral/.config/admiral.d/battery.sh
+++ admiral/.config/admiral.d/battery.sh
@@ -3,15 +3,20 @@
 
 . ~/.config/lime/colors.conf
 
+acstatus="$(apm -a)"
 batpercent="$(apm -l)"
 batstatus="$(apm -b)"
 
-if [ "${batstatus}" = '0' ]; then
-	battery="%{F$c2}${batpercent}%"
-elif [ "${batstatus}" = '1' ]; then
-	battery="%{F-}${batpercent}%"
-else
-	battery="%{F$c1}${batpercent}%"
+if [ "${acstatus}" = 1 ]; then
+	ac="%{F$c2}+"
 fi
 
-echo "%{B-}${battery}"
+if [ "${batstatus}" = '0' ]; then
+	battery="%{F$c2}${batpercent}"
+elif [ "${batstatus}" = '1' ]; then
+	battery="%{F-}${batpercent}"
+else
+	battery="%{F$c1}${batpercent}"
+fi
+
+echo "%{B-}%{F-}${ac}${battery}"
